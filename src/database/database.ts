fd2ac1b7fd3d1d8ab2fed6d2aa37c4793e4b9e0f
gnoronha42 <gabriel.noronha@creathus.org.br>
2023-06-23 06:02:07 -0400

    feat:database connect

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,7 +2,15 @@ import mongoose, { ConnectOptions } from "mongoose";
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/chip-paws-database");
+    const uri =
+      "mongodb://atlas-sql-6498566dc859636b4cbefad7-81djq.a.query.mongodb.net/chip-paws?ssl=true&authSource=admin";
+
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as unknown as ConnectOptions;
+
+    await mongoose.connect(uri, options);
     console.log("Conectado ao banco de dados");
   } catch (error) {
     console.error("Erro ao conectar ao banco de dados:", error);
